5eefba80ef0f1d99d4b3ccfa403ea0196a0286e6
Olexander Bryl <edlrian814@gmail.com>
2023-12-29 22:07:21 +0200

    Implement analisys part

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 DJINNI_URL = "https://djinni.co/jobs/keyword-python/"
+DJINNI_URL_JUNIOR = "https://djinni.co/jobs/?primary_keyword=Python&exp_rank=junior"
+DJINNI_URL_MIDDLE = "https://djinni.co/jobs/?primary_keyword=Python&exp_rank=middle"
+DJINNI_URL_SENIOR = "https://djinni.co/jobs/?primary_keyword=Python&exp_rank=senior"
+DJINNI_URL_TEAM_LEAD = "https://djinni.co/jobs/?primary_keyword=Python&exp_rank=team_lead"
+
+
 
 COLUMNS = [
     "pandas",
     "sql",
     "machine_learning",
+    "artificial_intelligence",
     "django",
     "fastapi",
     "flask",
@@ -23,7 +30,7 @@ COLUMNS = [
 class Vacancy:
     def __init__(self, text: str) -> None:
         for column in COLUMNS:
-            self.__setattr__(column, 1 if column in text else 0)
+            self.__setattr__(column, 1 if column.replace("_", " ") in text else 0)
 
     def __str__(self) -> str:
         return " ".join(self.__getattribute__(attr) for attr in COLUMNS)
